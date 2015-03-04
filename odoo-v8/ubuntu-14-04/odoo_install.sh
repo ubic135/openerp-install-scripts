3e6b610cc2eb684e3014bc7205196cee0adeaa7e
FRANCOIS DHOMMEAUX <fd@ubic.pro>
2015-03-04 01:19:09 +0100

    Update odoo_install.sh

--- odoo-v8/ubuntu-14-04/odoo_install.sh
+++ odoo-v8/ubuntu-14-04/odoo_install.sh
@@ -23,7 +23,7 @@ OE_HOME="/opt/$OE_USER"
 OE_HOME_EXT="/opt/$OE_USER/$OE_USER-server"
 
 #Enter version for checkout "8.0" for version 8.0, "7.0 (version 7), saas-4, saas-5 (opendays version) and "master" for trunk
-OE_VERSION="8.0"
+OE_VERSION="master"
 
 #set the superadmin password
 OE_SUPERADMIN="superadminpassword"
@@ -88,7 +88,7 @@ sudo ln -s /usr/bin/nodejs /usr/bin/nod
 #--------------------------------------------------
 # Install ODOO
 #--------------------------------------------------
-echo -e "\n==== Installing ODOO Server OCA-OCB ===="
+echo -e "\n==== Installing ODOO ===="
 sudo git clone --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
 echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
