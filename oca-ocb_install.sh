725d20a2a23374f815a38ab1c9229551b090ba3f
FRANCOIS DHOMMEAUX <fd@ubic.pro>
2015-04-13 14:24:12 +0200

    Update oca-ocb_install.sh

--- oca-ocb_install.sh
+++ oca-ocb_install.sh
@@ -202,6 +202,8 @@ cd //
 cd /opt/odoo/custom/addons
 unzip theme_bootswatch-master.1.0.zip
 
+echo -e "* Install Import bank ofx"
+ sudo git clone --branch 8.0 https://github.com/OCA/bank-statement-import.git /opt/odoo/gt-oca-bank-statement-import
+ sudo pip install ofxparse
 
- 
 echo "Done! The ODOO server can be started with /etc/init.d/$OE_CONFIG"
